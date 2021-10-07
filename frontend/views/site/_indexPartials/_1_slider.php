ef9204e7a9c198fb4f237a4711d8e83dae249298
isxoq <isxoqjon_7710@mail.ru>
2021-10-07 08:45:08 +0500

    fix admn left menu

--- frontend/views/site/_indexPartials/_1_slider.php
+++ frontend/views/site/_indexPartials/_1_slider.php
@@ -16,42 +16,7 @@ $sliders = \backend\modules\frontendmanager\models\CourseSlider::getDataForIndex
 <!-- 2nd Home Slider -->
 <div class="home1-mainslider">
     <div class="container-fluid p0">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="main-banner-wrapper" style="height: 450px">
-                    <div class="banner-style-one owl-theme owl-carousel">
-                        <?php foreach ($sliders as $slider): ?>
-
-                            <div class="slide slide-one"
-                                 style="background-image: url(<?= $slider['image'] ?>); height: 400px; padding-top: 100px">
-                                <div class="container">
-                                    <div class="row home-content" style="padding-top: 0">
-                                        <div class="col-lg-12 text-center p0">
-                                            <h4 class="banner-title"
-                                                style="padding: 0 10%; ">  <?= e($slider['translation']['title']) ?></h4>
-                                            <p> <?= e($slider['translation']['text']) ?></p>
-                                            <div class="btn-block">
-                                                <a href="<?= to(['course/detail', 'id' => $slider['course']['slug']]) ?>" class="banner-btn">
-                                                    <?= t('Read more') ?>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        <?php endforeach; ?>
-                    </div>
-                    <div class="carousel-btn-block banner-carousel-btn">
-                        <span class="carousel-btn left-btn" >
-                            <i class="flaticon-left-arrow left" style="font-size: 30px"></i>
-                        </span>
-                        <span class="carousel-btn right-btn" >
-                            <i class="flaticon-right-arrow-1 right" style="font-size: 30px"></i></span>
-                    </div><!-- /.carousel-btn-block banner-carousel-btn -->
-                </div><!-- /.main-banner-wrapper -->
-            </div>
-        </div>
+        <img style="height: 400px; width: fit-content" src="/images/land.jpg" alt="">
     </div>
 
 </div>

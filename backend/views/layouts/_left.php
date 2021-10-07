17f952258fea1ed890c6a04f5b0b104339dcec36
isxoq <isxoqjon_7710@mail.ru>
2021-10-07 07:24:44 +0500

    fix admn left menu

--- backend/views/layouts/_left.php
+++ backend/views/layouts/_left.php
@@ -18,16 +18,16 @@ $menuItems = [
         'icon' => 'settings,feather',
         'items' => [
             ['label' => "Asosiy ma'lumotlar", 'url' => ['/settings/manager']],
-            ['label' => 'OCTO UZ', 'url' => ['/octouz/setting/index']],
-            ['label' => 'OCTO Transactions', 'url' => ['/octouz/octo-transactions/index']],
+//            ['label' => 'OCTO UZ', 'url' => ['/octouz/setting/index']],
+//            ['label' => 'OCTO Transactions', 'url' => ['/octouz/octo-transactions/index']],
             ['label' => 'Menu', 'url' => ['/menumanager']],
             ['label' => 'Tarjimalar', 'url' => ['/translate-manager']],
             ['label' => 'SMS sozlamalari', 'url' => ['/smsmanager/default/settings']],
-            ['label' => 'Bizning jamoa', 'url' => ['/team']],
-            ['label' => 'Socials', 'url' => ['/socialmanager']],
+//            ['label' => 'Bizning jamoa', 'url' => ['/team']],
+//            ['label' => 'Socials', 'url' => ['/socialmanager']],
             ['label' => 'Mijozlar fikri', 'url' => ['/testimonial']],
-            ['label' => 'Hamkorlar', 'url' => ['/partner']],
-            ['label' => 'Imkoniyatlar', 'url' => ['/frontendmanager/index-info']],
+//            ['label' => 'Hamkorlar', 'url' => ['/partner']],
+//            ['label' => 'Imkoniyatlar', 'url' => ['/frontendmanager/index-info']],
             ['label' => "Ta'lim darajalari", 'url' => ['/education-level']],
         ]
     ],
@@ -45,29 +45,29 @@ $menuItems = [
     ],
     ['label' => 'Kurs comments', 'url' => ['/kursmanager/kurs-comment'], 'badgeLabel' => $newCommentsCount, 'badgeClass' => 'danger', 'icon' => 'message-circle,feather'],
 
-    [
-        'label' => 'Billing',
-        'icon' => 'book,feather',
-        'items' => [
-            ['label' => 'Sotib olishlar', 'url' => ['/billing/purchases/index']],
-            [
-                'label' => "Offline to'lovlar",
-                'icon' => 'book,feather',
-                'items' => [
-                    ['label' => "Ro'yhat", 'url' => ['/billing/offline-payments/index']],
-                    ['label' => "Yangi qo'shish", 'url' => ['/billing/offline-payments/create']],
-                ]
-            ],
-            [
-                'label' => "O'qituvchi to'lovlari",
-                'icon' => 'book,feather',
-                'items' => [
-                    ['label' => "Ro'yhat", 'url' => ['/billing/payouts/index']],
-                    ['label' => "Yangi qo'shish", 'url' => ['/billing/payouts/create']],
-                ]
-            ],
-        ]
-    ],
+//    [
+//        'label' => 'Billing',
+//        'icon' => 'book,feather',
+//        'items' => [
+//            ['label' => 'Sotib olishlar', 'url' => ['/billing/purchases/index']],
+//            [
+//                'label' => "Offline to'lovlar",
+//                'icon' => 'book,feather',
+//                'items' => [
+//                    ['label' => "Ro'yhat", 'url' => ['/billing/offline-payments/index']],
+//                    ['label' => "Yangi qo'shish", 'url' => ['/billing/offline-payments/create']],
+//                ]
+//            ],
+//            [
+//                'label' => "O'qituvchi to'lovlari",
+//                'icon' => 'book,feather',
+//                'items' => [
+//                    ['label' => "Ro'yhat", 'url' => ['/billing/payouts/index']],
+//                    ['label' => "Yangi qo'shish", 'url' => ['/billing/payouts/create']],
+//                ]
+//            ],
+//        ]
+//    ],
 
 
     ['label' => 'Sahifalar', 'icon' => 'list,feather', 'url' => ['/pagemanager']],
@@ -81,25 +81,25 @@ $menuItems = [
         ]
     ],
 
-    [
-        'label' => 'Arizalar',
-        'icon' => 'edit,feather',
-        'url' => ['/usermanager/teacher-application'],
-        'badgeLabel' => $newAppliesCount,
-        'badgeClass' => 'danger',
-    ],
+//    [
+//        'label' => 'Arizalar',
+//        'icon' => 'edit,feather',
+//        'url' => ['/usermanager/teacher-application'],
+//        'badgeLabel' => $newAppliesCount,
+//        'badgeClass' => 'danger',
+//    ],
 
     ['label' => 'Foydalanuvchilar', 'icon' => 'user,feather', 'url' => ['/usermanager/user']],
     ['label' => 'Foyd. tarixi', 'icon' => 'users,feather', 'url' => ['/usermanager/user-history']],
 
-    [
-        'label' => 'FAQ',
-        'icon' => 'help-circle,feather',
-        'items' => [
-            ['label' => 'FAQ kategoriyalari', 'url' => ['/faqmanager/faq-category']],
-            ['label' => 'FAQ savollar', 'url' => ['/faqmanager/faq']],
-        ]
-    ],
+//    [
+//        'label' => 'FAQ',
+//        'icon' => 'help-circle,feather',
+//        'items' => [
+//            ['label' => 'FAQ kategoriyalari', 'url' => ['/faqmanager/faq-category']],
+//            ['label' => 'FAQ savollar', 'url' => ['/faqmanager/faq']],
+//        ]
+//    ],
 
 
     ['label' => 'Buyurtmalar', 'icon' => 'edit,feather', 'url' => ['/ordermanager/order']],
@@ -107,24 +107,24 @@ $menuItems = [
 //    ['label' => 'Gii', 'icon' => 'grid,feather', 'url' => ['/gii']],
 
 
-    [
-        'label' => "Advanced Custom Fields",
-        'icon' => 'acquisitions-incorporated,fab',
-        'items' => [
-            ['label' => "ACF Category", 'url' => ['/acf/field-type']],
-            ['label' => "ACF fields", 'url' => ['/acf/field']],
-        ]
-    ],
-    [
-        'label' => 'Bot',
-        'icon' => 'settings,feather',
-        'items' => [
-            ['label' => "Ro'yhatdan o'tganlar", 'url' => ['/botmanager/bot-user']],
-            ['label' => 'Xabar yuborish', 'url' => ['/botmanager/bot-user/send']],
-            ['label' => 'Zakazlar', 'url' => ['/botmanager/bot-user-action']],
-
-        ]
-    ],
+//    [
+//        'label' => "Advanced Custom Fields",
+//        'icon' => 'acquisitions-incorporated,fab',
+//        'items' => [
+//            ['label' => "ACF Category", 'url' => ['/acf/field-type']],
+//            ['label' => "ACF fields", 'url' => ['/acf/field']],
+//        ]
+//    ],
+//    [
+//        'label' => 'Bot',
+//        'icon' => 'settings,feather',
+//        'items' => [
+//            ['label' => "Ro'yhatdan o'tganlar", 'url' => ['/botmanager/bot-user']],
+//            ['label' => 'Xabar yuborish', 'url' => ['/botmanager/bot-user/send']],
+//            ['label' => 'Zakazlar', 'url' => ['/botmanager/bot-user-action']],
+//
+//        ]
+//    ],
 
 
     ['label' => 'Clear cache', 'icon' => 'umbrella,feather', 'url' => ['/site/cache-flush']],
